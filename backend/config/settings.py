6decc61ee5a5acef786232bb6fc9f3d11a6ac8c2
zeeglyismail <zeeglyismail@gmail.com>
2025-11-14 15:27:44 +0600

    db dump

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-dev-key')
 
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0', cast=Csv())
 
 
 DJANGO_APPS = [
